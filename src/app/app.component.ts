700a3c0e4d524bad077f1b53751fa9d543ac8d70
DarkHotTea <105120676+DarkHotTea@users.noreply.github.com>
2024-12-24 15:43:21 +0300

    Hello world

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [],
+  template: ` <h1>Hello world!</h1> `,
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'CM_Proj-7_HM';
+  title = 'homes';
 }
