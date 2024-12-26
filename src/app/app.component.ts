0acf1854429c4825c9cc49fbc0b9c8c0efc8ff7f
DarkHotTea <105120676+DarkHotTea@users.noreply.github.com>
2024-12-26 23:27:44 +0300

    Add dynamic values to templates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { HomeComponent } from './home/home.component';
   standalone: true,
   imports: [HomeComponent],
   template: `
-    <main>
+    <main class="h-full grid grid-rows-[auto_1fr]">
       <header class="brand-name flex items-center gap-1">
         <img
           class="brand-logo max-w-14"
@@ -16,12 +16,15 @@ import { HomeComponent } from './home/home.component';
         />
         <span class="text-4xl font-semibold">Homes</span>
       </header>
-      <section class="content">
+      <section class="content bg-slate-100">
         <app-home></app-home>
       </section>
     </main>
   `,
   styleUrls: ['./app.component.scss'],
+  host: {
+    class: 'app-root',
+  },
 })
 export class AppComponent {
   title = 'homes';
