de492d0353cf46b2fa16829cb3dfbfe54d6c1816
DarkHotTea <105120676+DarkHotTea@users.noreply.github.com>
2024-12-28 02:49:16 +0300

    Add search functionality

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,13 +11,19 @@ import { HousingService } from '../housing-location/housing.service';
   template: `
     <section>
       <form>
-        <input type="text" placeholder="Filter by city" />
-        <button class="primary" type="button">Search</button>
+        <input type="text" #filter placeholder="Filter by city" />
+        <button
+          class="primary"
+          type="button"
+          (click)="filterResults(filter.value)"
+        >
+          Search
+        </button>
       </form>
     </section>
     <section class="results">
       <app-housing-location
-        *ngFor="let housingLocation of housingLocationList"
+        *ngFor="let housingLocation of filteredLocationList"
         [housingLocation]="housingLocation"
       />
     </section>
@@ -26,9 +32,23 @@ import { HousingService } from '../housing-location/housing.service';
 })
 export class HomeComponent {
   housingLocationList: IHousingLocation[] = [];
-  housingService: HousingService = inject(HousingService); 
+  housingService: HousingService = inject(HousingService);
+  filteredLocationList: IHousingLocation[] = [];
 
   constructor() {
     this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.filteredLocationList = this.housingLocationList;
+  }
+
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+      return;
+    }
+
+    this.filteredLocationList = this.housingLocationList.filter(
+      (housingLocation) =>
+        housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+    );
   }
 }
